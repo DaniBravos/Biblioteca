00bc4ba2f7f2c40195b34ec904429847769a8ace
Daniel Bravo <dani.bravos@duocuc.cl>
2025-04-24 17:38:08 -0400

    Cambios 24/04/2025

--- src/main/java/com/example/biblioteca/controller/LibroController.java
+++ src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -7,16 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+import java.util.List;
 
 
 @RestController
@@ -52,6 +54,14 @@ public class LibroController {
         return libroService.guardarLibro(libro);
     }
     
+    @PutMapping("{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro) {
+        return libroService.actualizarLibro(libro);
+    }
     
-
+    @DeleteMapping("{id}")
+     public String eliminarLibro(@PathVariable int id){
+        libroService.eliminarLibro(id);
+        return "Libro Eliminado";
+    }
 }
